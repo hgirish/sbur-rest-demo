d626b722ec908c9fa11197febb0063f701bc90fc
hgirish <girish@post.com>
2021-09-25 16:02:42 -0700

    Initial Project created by start.spring.io

---
+++ src/test/java/com/gohands/sburrestdemo/SburRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gohands.sburrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
